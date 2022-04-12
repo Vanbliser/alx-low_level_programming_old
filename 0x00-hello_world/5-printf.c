fce228ba803ff224ab607e2a692ec1d9cd2f3a96
Vanbliser <ayogublossomisrael@gmail.com>
2022-04-12 13:55:45 +0000

    Added Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* Program begins here*/
+/**
+* main - Program begins
+* Return: returns int 0
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
