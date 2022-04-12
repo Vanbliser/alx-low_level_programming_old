fa75a70600577af60457874aebbd06ea4e0cd758
Vanbliser <ayogublossomisrael@gmail.com>
2022-04-12 12:00:55 +0000

    Added 5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/* Program begins here */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Program begins here*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
