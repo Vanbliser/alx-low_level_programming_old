83a7c34fc0e32c864a0b657130856d4a64c77968
Vanbliser <ayogublossomisrael@gmail.com>
2022-04-14 15:54:02 +0000

    Added 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,7 +5,7 @@
  * of base 10 starting from 0, followed by a new line.
  *
  * Description: You are not allowed to use any variable of type char
- * You can only use the putchar function 
+ * You can only use the putchar function
  * (every other function (printf, puts, etc…) is forbidden)
  * You can only use putchar twice in your code
  * All your code should be in the main function
