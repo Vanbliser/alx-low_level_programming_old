3e25aba11fe063eff605db28211537924950e722
Vanbliser <ayogublossomisrael@gmail.com>
2022-04-15 22:17:32 +0000

    Added 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,30 @@
  */
 int print_last_digit(int i)
 {
-	int mod;
+	int last_digit;
+	int int_char;
 
 	if (i < 0)
 		i *= -1;
-	mod = 48 + i % 10;
-	_putchar(mod);
-	return (mod);
+	last_digit = i % 10;
+	int_char = 48 + last_digit;
+	_putchar(int_char);
+	return (last_digit);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
 }
