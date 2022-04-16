6bc6a101002f55f5029540c3a188bdecd78162c2
Vanbliser <ayogublossomisrael@gmail.com>
2022-04-16 15:30:28 +0000

    Added 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,6 +7,7 @@
  * Description: If we list all the natural numbers below 10 that are
  * multiples of 3 or 5, we get 3, 5, 6 and 9.
  * The sum of these multiples is 23.
+ * Return: return 0 on success.
  */
 int main(void)
 {
