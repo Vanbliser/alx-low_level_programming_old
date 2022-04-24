d9b65ac7a917835242723fe1629329c63a12290b
Vanbliser <ayogublossomisrael@gmail.com>
2022-04-24 10:28:22 +0000

    Added 100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _atoi -  a function that convert a string to an integer.
  * @s: the string to be converted
@@ -11,20 +12,73 @@ int _atoi(char *s)
 	int j = 0;
 	int ans = 0;
 	int sign = 1;
-
+	char c;
 	for (i = 0; *(s + i) != '\0'; ++i)
 	{
-		if (*(s + i) >= 48 && *(s + i) <= 57)
-		{
-			++j;
-			ans = (ans * 10) + (int)(*(s + i));
-		}
-		if (*(s + i) == 45)
+		c = *(s + i);
+		if (c == 45)
 			sign = sign * -1;
-		if (*(s + i) == 43)
+		if (c == 43)
 			sign = sign * 1;
-		if ((j > 0) && (*(s + i) == ' ' || *(s + i) == '	'))
+		if ((j > 0) && (c == ' ' || c == '	'))
 			break;
+		if (c >= 48 && c <= 57)
+		{
+			while(c >= 48 && c <= 57)
+			{
+				ans = (ans * 10) + char_to_int(c);
+				c = *(s + ++i);
+			}
+			break;
+		}
+		if (c == '\0')
+			return (0);
 	}
 	return (sign * ans);
+}
+
+/**
+ * char_to_int - a function that converts a character type number to an
+ * integer type number
+ * @i: the character type number
+ *
+ * Return: integer type number 
+ */
+int char_to_int(char i)
+{
+	int ans;
+	switch (i)
+	{
+	case '0':
+		ans = 0;
+		break;
+	case '1':
+		ans = 1;
+		break;
+	case '2':
+		ans = 2;
+		break;
+	case '3':
+		ans = 3;
+		break;
+	case '4':
+		ans = 4;
+		break;
+	case '5':
+		ans = 5;
+		break;
+	case '6':
+		ans = 6;
+		break;
+	case '7':
+		ans = 7;
+		break;
+	case '8':
+		ans = 8;
+		break;
+	case '9':
+		ans = 9;
+		break;
+	}
+	return (ans);
 }
