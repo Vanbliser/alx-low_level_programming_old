4b8716da779b88839a9db9a55cc0f3ca489ce578
Vanbliser <ayogublossomisrael@gmail.com>
2022-04-24 16:25:19 +0000

    Added 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 /**
  * _isupper - a function that checks for uppercase character.
  * @c:
- * 
- * Return:  return 1 if uppercase and 0 if not. 
+ *
+ * Return:  return 1 if uppercase and 0 if not.
  */
 int _isupper(int c)
 {
