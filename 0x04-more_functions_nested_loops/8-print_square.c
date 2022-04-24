52f43a830310cabf63201b6f0ccfdd8e1c0c0c57
Vanbliser <ayogublossomisrael@gmail.com>
2022-04-24 19:55:16 +0000

    Added 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,10 +12,10 @@ void print_square(int size)
 		for (i = 1; i <= size; ++i)
 		{
 			for (j = 1; j <= size; ++j)
-			{
 				_putchar('#');
-			}
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
