6858a5ed3dab7f198b26bf12df1ec5a4dd3242d1
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-02 19:50:02 +0000

    Added 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,13 +3,6 @@
  * leet - a function that encodes a string into 1337.
  * @str: the string to encode
  *
- * Description: Letters a and A should be replaced by 4
- * Letters e and E should be replaced by 3
- * Letters o and O should be replaced by 0
- * Letters t and T should be replaced by 7
- * Letters l and L should be replaced by 1
- * You can only use one if statement and two loop statements in your code.
- * You are not allowed to use switch and ternary operation
  * Return: the encoded string
  */
 char *leet(char *str)
