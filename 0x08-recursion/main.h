18fac560bb1bdc9825b25e7557b6b946b7b50733
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-05 06:56:48 +0000

    Added 1-print_rev_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,5 +3,8 @@
 
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int length_of_string(char *s, int i);
+void print_recursively(char *s, int i);
 
 #endif /* MAIN */
