6e5532d68b1e42e6953de982cfe745dbe880b0d2
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-05 07:05:00 +0000

    Added 2-strlen_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,5 +6,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int length_of_string(char *s, int i);
 void print_recursively(char *s, int i);
+int _strlen_recursion(char *s);
 
 #endif /* MAIN */
