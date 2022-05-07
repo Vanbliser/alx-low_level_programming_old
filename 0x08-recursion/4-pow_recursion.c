dc35290c2da66aeb0ae6d6aa606cddc0fea787c6
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-07 06:00:24 +0000

    Added 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion - a function that returns the value of x 
+ * _pow_recursion - a function that returns the value of x
  * raised to the power of y.
  * @x: the base
  * @y: the ezponent
@@ -15,5 +15,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	if (y == 1)
 		return (x);
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 }
