c871de1ab1901a971675f203cfe0a99a54119f13
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-07 09:20:45 +0000

    Added 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * is_prime_number - a function that returns 1 if the input integer
+ * is a prime number, otherwise return 0.
+ * @n: the input integer
+ *
+ * Return: Returns 1 if it's a prime number
+ */
+int is_prime_number(int n)
+{
+	return (_prime(2, n));
+}
+/**
+ * _prime - a function that returns 1 if the input integer
+ * is a prime number, otherwise return 0.
+ * @n: divisor starting at 2 and incrementing at each recursion
+ * @k: the number
+ *
+ * Return: int 1 or 0
+ */
+int _prime(int n, int k)
+{
+	k = (k < 0) ? k * -1 : k;
+
+	if ((k != n) && (k == 1 || k == 0 || k % n == 0))
+	{
+		return (0);
+	}
+	else if (k == n)
+	{
+		return (1);
+	}
+	else
+	{
+		++n;
+		return (_prime(n, k));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int double_up(int n, int k);
+int is_prime_number(int n);
+int _prime(int n, int k);
 
 #endif /* MAIN */
