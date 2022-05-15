2a5386771cdc123da14fbb1606a9bc8908459917
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-15 06:12:35 +0000

    Added 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,7 +6,7 @@
  * Only print one argument per line, ending with a new line
  * @argc: no of attribute
  * @argv: list of arguments passed as string
- * 
+ *
  * Return: Return 0 on success
  */
 int main(int argc, char __attribute__ ((unused)) **argv)
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char **argv)
+{
+
+	return (0);
+}
