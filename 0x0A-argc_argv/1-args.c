5b98a593cc97283a49b02da352465cb728884193
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-15 06:01:11 +0000

    Added 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * main - Your program should print a number, followed by a new line
+ * @argc: the number of arguments passed to main
+ * @argv: the list of strings arguments to be printed.
+ *
+ * Return: return 0 un success.
+ */
 int main(int argc, char __attribute__ ((unused)) **argv)
 {
 	printf("%d\n", argc - 1);
