1b2c942fb69938ec528c1e69a9a0a8fa8ead0b87
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-15 06:09:49 +0000

    Added 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program that prints all arguments it receives.
+ * All arguments should be printed, including the first one
+ * Only print one argument per line, ending with a new line
+ * @argc: no of attribute
+ * @argv: list of arguments passed as string
+ * 
+ * Return: Return 0 on success
+ */
+int main(int argc, char __attribute__ ((unused)) **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; ++i)
+	{
+		printf("%s\n", *(argv + i));
+	}
+	return (0);
+}
