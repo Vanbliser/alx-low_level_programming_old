527b2adf0f88a7ee10a10da596d01b030d92a1f5
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-15 06:22:05 +0000

    Added 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - 
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ * main - a program that multiplies two numbers.
+ * Your program should print the result of the multiplication, followed by a
+ * new line. You can assume that the two numbers and result of the 
+ * multiplication can be stored in an integer. If the program does not receive
+ * two arguments, your program should print Error, followed by a new line, and
+ * return 1.
+ * @argc: the number of arguments passed to the program
+ * @argv: list of arguments passed as string
+ *
+ * Return: return 0 on success
  */
-int main(int argc, char **argv)
+int main(int __attribute__ ((unused)) argc, char **argv)
 {
-
-	return (0);
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(*(argv + 1)) * atoi(*(argv + 2)));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
