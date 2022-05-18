646c2f9f2685322ab197bd4b689f8877d29c6c4a
Vanbliser <ayogublossomisrael@gmail.com>
2022-05-18 09:38:51 +0000

    Added 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,22 +14,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int num =  atoi(argv[1]);
-	int result = num;
-	int i;
+	int num, result, i;
 
-	if (argc == 1)
+	if (argc == 1 || atoi(argv[1]) == 0)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (num <= 0)
+	else if (atoi(argv[1]) < 0)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
 	else
 	{
+		num =  atoi(argv[1]);
+		result = num;
+
 		for (i = 0; result != 0; ++i)
 		{
 			result -= closest_to(result);
