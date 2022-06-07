706f0e5e44d25730462ace9afe8261e43fc92712
Vanbliser <ayogublossomisrael@gmail.com>
2022-06-07 20:17:03 +0000

    Added 1-string_nconcat.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-int _stderr(char c, char d);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+unsigned int _strlen(char *str);
 
 #endif /* end MAIN */
