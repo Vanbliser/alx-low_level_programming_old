9856cf98a264ba99b1d785f022be9e99c28ecb42
Vanbliser <ayogublossomisrael@gmail.com>
2022-06-07 13:50:19 +0000

    Added fixed betty error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,7 +6,7 @@
  * if malloc fails, the malloc_checked function should cause normal process
  * termination with a status value of 98
  * @b: the size of the memory
- * 
+ *
  * Return: Returns a pointer to the newly created memory
  */
 void *malloc_checked(unsigned int b)
@@ -15,8 +15,8 @@ void *malloc_checked(unsigned int b)
 
 	if (ptr == NULL)
 	{
-		exit (98);
+		exit(98);
 	}
-	
+
 	return (ptr);
 }
