1f316c68af09af61b54e93d62a1fd0543e67dca3
Vanbliser <ayogublossomisrael@gmail.com>
2022-06-08 14:08:15 +0000

    Added fixed betty bug

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * array_range - a function that creates an array of integers.
  * The array created should contain all the values from min (included) to max
- * (included), ordered from min to max
- * Return: the pointer to the newly created array
- * If min > max, return NULL
- * If malloc fails, return NULL
+ * (included), ordered from min to max Return: the pointer to the newly created
+ * array. If min > max, return NULL If malloc fails, return NULL
  * @min: the minimum value the array should contain
  * @max: the maximum value the array should contain
  *
- * Return: return a pointer to the array of integers
+ * Return: a pointer to the array of integers
  */
+
 int *array_range(int min, int max)
 {
 
