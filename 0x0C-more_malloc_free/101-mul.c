4394d8fa8d7097fe2753374f51d871475a1e129a
Vanbliser <ayogublossomisrael@gmail.com>
2022-06-10 15:36:54 +0000

    Added fixed multiply by zero error

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	unsigned int i, j, k, x, y, z, l1, l2, **pt1, **pt2;
 	char *int1, *int2;
 
-	if (argc > 3 || _is_not_digit(argv[1]) || _is_not_digit(argv[2]))
+	if (argc > 3 || argc < 3|| _is_not_digit(argv[1]) || _is_not_digit(argv[2]))
 		error();
 	l1 = length(argv[1]);
 	l2 = length(argv[2]);
@@ -51,12 +51,12 @@ int main(int argc, char **argv)
 	}
 	addition(pt1, k, l2);
 	for (j = 0; j <= k; ++j)
-		if (*(*(pt1 + l2) + j) != 0)
+		if (*(*(pt1 + l2) + j) != 0 || j == k)
 			for (; j <= k; ++j)
 				_putchar(_itoa(*(*(pt1 + l2) + j)));
 	_putchar('\n');
 	_free(pt1, l2);
-	return (0);
+	exit (0);
 }
 /**
  * _free - a function that frees all allocated memory stored in an array
