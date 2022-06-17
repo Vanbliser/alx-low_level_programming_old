c1432d3d337f905c2ecafeee2059b150d12c4ae0
Vanbliser <ayogublossomisrael@gmail.com>
2022-06-17 13:50:53 +0000

    fixed copy of string error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - a function that creates a new dog.
  * @name: the name of the dog
@@ -15,7 +16,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *dog_name;
 	char *dog_owner;
 
-
+	dog_name = malloc(sizeof(name));
+	dog_owner = malloc(sizeof(owner));
 	my_dog = malloc(sizeof(dog_t));
 	if (name == NULL || owner == NULL || my_dog == NULL)
 	{
@@ -23,8 +25,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	else
 	{
-		dog_name = name;
-		dog_owner = owner;
+		strcpy(dog_name, name);
+		strcpy(dog_owner, owner);
 	}
 
 	my_dog->name = dog_name;
