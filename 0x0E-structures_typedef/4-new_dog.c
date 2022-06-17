8dc40d5de77cb593f9ece2b004bec7be7c79188b
Vanbliser <ayogublossomisrael@gmail.com>
2022-06-17 13:20:01 +0000

    memory error test1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - a function that creates a new dog.
  * @name: the name of the dog
@@ -16,14 +17,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *dog_owner;
 
 	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
+		free(my_dog);
+
+	dog_name = malloc(sizeof(name));
+	if (dog_name == NULL)
+	{
+		free(dog_name);
+		free(my_dog);
+	}
+
+	dog_owner = malloc(sizeof(owner));
+	if (dog_owner == NULL)
+	{
+		free(dog_owner);
+		free(dog_name);
+		free(my_dog);
+	}
+
 	if (name == NULL || owner == NULL || my_dog == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-		dog_name = name;
-		dog_owner = owner;
+		strcpy(dog_name, name);
+		strcpy(dog_owner, owner);
 	}
 
 	my_dog->name = dog_name;
