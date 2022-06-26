82e2704973b97ff32436f72e28d3e238eef00106
Vanbliser <ayogublossomisrael@gmail.com>
2022-06-26 14:39:50 +0000

    Fixed betty error in 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	char *string;
 	unsigned int index;
-    va_list args;
+	va_list args;
 
 	va_start(args, n);
 
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(args, char *);
 
 		if (!string)
-            printf("(nil)");
+			printf("(nil)");
 		else
 			printf("%s", string);
 
