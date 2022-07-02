89b058e0f9e0eb6504f7ddac2d05ab43f6bed829
Vanbliser <ayogublossomisrael@gmail.com>
2022-07-02 16:49:05 +0000

    Fixed 3-add_nodeint_end.c error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * @add_nodeint_end - a function that adds a new node at the end of a
+ * add_nodeint_end - a function that adds a new node at the end of a
  * listint_t list.
  * @head: a pointer to a pointer to a listint_t list
  * @n: an integer value
@@ -10,21 +10,23 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
+	listint_t *tmp;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
+	tmp = *head;
 	if (*head == NULL)
 	{
 		*head = new;
 	}
 	else
 	{
-		while ((*head)->next != NULL)
-			(*head) = (*head)->next;
-		(*head)->next = new;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
 	}
 	return (*head);
 }
